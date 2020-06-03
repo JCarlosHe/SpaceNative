27206134ea0863d8ac44d7b099a0c636aeb3571d
carlos heredia <jcarlos3004@me.com>
2020-06-03 10:21:38 -0500

    first commit

---
+++ src/main/java/org/codigorupestre/Spacenative/App.java
@@ -0,0 +1,13 @@
+package org.codigorupestre.Spacenative;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
